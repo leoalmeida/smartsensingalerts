8446c14bb79dbbaeb8d8e3af52c835ee5ff73776
Leonardo Almeida silva ferreira <leoalmeida.rj@gmail.com>
2016-07-31 23:59:02 -0300

    fix env configs

--- src/app/shared/constant/env.model.ts
+++ src/app/shared/constant/env.model.ts
@@ -1,6 +1,6 @@
 /**
  * Only valid JSON data types can be used for types
-                                               + * * Make sure the keys in `env.model.ts` exist in `env.json`
+ * * Make sure the keys in `env.model.ts` exist in `env.json`
  * otherwise it'll throw message like this
  * Property '<missing-key>' is missing in type '{}'
  *
